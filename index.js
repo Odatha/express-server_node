20277536b9552867be347db50ffc056e60e33096
Odatha <bandaraods@gmail.com>
2020-11-08 09:32:46 +0530

    express server built

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const express = require("express");
 const http = require("http");
+const morgan = require("morgan");
 
 const hostname = "localhost";
 const port = 3000;
 
-const app = express();
+const app = express(); //use express module
+app.use(morgan("dev")); //print out additional information to the screen
+
+app.use(express.static(__dirname + "/public")); //express you will look up root of this project
 
 app.use((req, res, next) => {
-  console.log(req.headers);
+  //next is used to invoke additional middleware
+
   res.statusCode = 200;
   res.setHeader("Content-type", "text/html");
   res.end("<html><body><h1>This is an express server</h1></body></html>");
 });
 
-const server = http.createServer(app);
+const server = http.createServer(app); //setup the server
 
 server.listen(port, hostname, () => {
   console.log(`server runnig at http://${hostname}:${port}`);
